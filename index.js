a79babea77de2a74b943c1332a9f5eb1ba5955f6
Michael Hellein <themichaek@gmail.com>
2016-07-15 22:15:54 -0400

    Initial AST transformation.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var markdownIt = require('markdown-it');
 var highlight = require('highlight.js');
+var htmlparser = require('htmlparser2');
+var domutils = require('domutils');
 
+var wrapPrefix = 'markdeck-';
+var tags = ['page', 'section', 'content'];
+var wrappers = tags.map(function(wrap) {
+	wrap = wrapPrefix+wrap;
+	return '<'+wrap+'></'+wrap+'>';
+}).join('')
 
 function parse(md, options) {
 	options = options || {};
@@ -9,7 +17,20 @@ function parse(md, options) {
 		return highlight.highlight(lang, code).value;
 	};
 
-	return markdownIt(options).render(md);
+	var html = markdownIt(options).render(md);
+	var handler = new htmlparser.DomHandler(function (error, dom) {
+		tags.map(function(tag){
+			var el = domutils.find(function(el){ return el.name === wrapPrefix+tag }, dom, true);
+			domutils.removeElement(el);
+			return el;
+		});
+	});
+
+	var parser = new htmlparser.Parser(handler);
+	parser.write('<'+wrapPrefix+'-presentation>'+html+wrappers+'<'+wrapPrefix+'-presentation>');
+	parser.done();
+
+	return html;
 }
 
 
---
+++ test/fixtures/pages.md
@@ -0,0 +1,22 @@
+# Title
+
+## Page 1
+
+Page one content.
+
+## Page 2
+
+Page two content.
+
+---
+
+Page two continued.
+
+## Page 3
+
+### Section 1
+
+### Section 2
+
++++++
+<h1>Title
